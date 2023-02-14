028a61ee38ad097e58dfe381f5cfa834c663f636
张营磊 <zhyinglei@szlanyou.com>
2023-02-14 08:41:32 +0800

    增加迭代器

--- 3/aboutString.h
+++ 3/aboutString.h
@@ -1,10 +1,32 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include <string>
 
+using std::vector;
+using std::string;
 
 void aboutString()
 {
+    vector<int> vecInt{1,2,3,4};
+    for (auto& item : vecInt)
+    {
+        if (item == 4)
+        {
+            vecInt.push_back(5);
+        }
+        std::cout << item << std::endl;
+    }
+
+
+    vector<string> vecStr;
+    vector<string>::iterator iter = vecStr.begin();
+
+    const vector<string> cVecStr;
+    vector<string>::const_iterator iterC = cVecStr.begin();
+    //vector<string>::iterator iter3 = cVecStr.cbegin();
+
     std::string s;
     std::cin >> s;
     std::cout << s << std::endl;
