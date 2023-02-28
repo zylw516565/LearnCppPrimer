c2d67998ce54b7aef66a88526c06424b456c7402
张营磊 <zhyinglei@szlanyou.com>
2023-02-28 10:41:35 +0800

    合成默认构造函数的试验

--- 7/chapter_7.h
+++ 7/chapter_7.h
@@ -9,12 +9,24 @@ using std::string;
 
 class Chapter7
 {
+private:
+    int value;
+    int *ptr;
+    string str;
 public:
 //     Chapter7()const
 //     {}
 
-    Chapter7()
-    {}
+     Chapter7(int tmp)
+     {}
+
+};
+
+class A
+{
+private:
+    int a;
+    Chapter7 chapter7;
 
 };
 
@@ -25,5 +37,10 @@ void readStream(std::istream& is)
 void chapter_7()
 {
     readStream(std::cin);
+    int tmp;
+    int *pTmp;
+    cout << "tmp = " << tmp << " pTmp = " << pTmp << endl;
+
+    //A a;
 
 }
