083c97be912f099d8cede6aace73dd56754fd290
张营磊 <zhyinglei@szlanyou.com>
2023-03-20 09:06:44 +0800

    WithArray

--- 13/chapter_13.h
+++ 13/chapter_13.h
@@ -26,11 +26,31 @@ public:
     int n = 0;
 };
 
+struct WithArray
+{
+//     explicit WithArray(const int a[])
+//         :array_(a)
+//     {}
+
+    int array_[5] = {0};
+};
+
+
 void testSynthesizedCopyConstructor()
 {
     AAA a1(2);
     AAA a2(a1);
     cout << a1.n << " " << a2.n << endl;
+
+    WithArray Array2;
+    cout << Array2.array_  << endl;
+    for (auto& elment : Array2.array_)
+    {
+        elment = 1;
+    }
+
+    WithArray Array3(Array2);
+    cout << Array2.array_ << " " << Array3.array_ << endl;
 }
 
 void chapter_13()
